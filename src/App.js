b67a082e6a6bd4bbe253a829075a3807846fdd14
Hussein Salloum <Hus8sein.Sal7loum@gmail.com>
2019-06-22 15:24:46 +0300

    Testing Material UI Layout and Card And Icons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,73 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container/Container';
+import Grid from '@material-ui/core/Grid/Grid';
+import Box from '@material-ui/core/Box';
 
-import './App.css';
+import ButtonAppBar from './components/AppNavBar';
+import SimpleCard from './components/Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-      </header>
-    </div>
+
+    <React.Fragment>
+        
+          <ButtonAppBar />
+          <br />
+          <br />
+
+
+          <Box color="text.primary" clone>
+              <Grid
+                container
+                direction="row"
+                justify="space-around"
+                alignItems="center"
+              >
+  
+                <Button variant="contained" color="primary">
+                  Hello World
+                </Button>
+              
+                <Button variant="contained" color="info">
+                  Hello World
+                </Button>
+
+                <Button variant="contained" color="success">
+                  Hello World
+                </Button>
+              
+                <Button variant="contained" color="danger">
+                  Hello World
+                </Button>
+            </Grid>
+          </Box>
+
+          <br />
+          <br />
+
+        <React.Fragment>
+        <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+              >
+                  <SimpleCard />
+                  <SimpleCard />
+              </Grid>
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+              >
+                  <SimpleCard />
+                  <SimpleCard />
+              </Grid>
+        </React.Fragment>
+
+        </React.Fragment>
   );
 }
 
